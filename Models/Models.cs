ec8f4b53ef9460e3c29a10273511ef8880728259
Oscar Durán <odurancuello@gmail.com>
2021-07-20 15:18:33 -0400

    Finished project

---
+++ Models/Models.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Biblioteca.Models
+{
+    public class Models
+    {
+        public Book book { get; set; }
+        public User user { get; set; }
+        public Comment comment { get; set; }
+        public Valoration valoration { get; set; }
+    }
+}
